f8fd90802e83afd7b4cf963340503987019b368e
maxdefanti <138813698+maxdefanti@users.noreply.github.com>
2024-08-01 16:05:06 -0600

    Update lineIntersection.py

--- lineIntersection.py
+++ lineIntersection.py
@@ -16,7 +16,7 @@ class Line:
         self.intercept = y0 - self.slope*x0
 
     def __str__(self):
-        return ("radius: " + str(self.r) + " theta: " + "{:.4f}".format(self.theta))
+        return ("radius: " + str(self.r) + " theta: " + "{:.2f}".format(self.theta*180/math.pi) + " degrees")
 
     def getx0(self):
          return self.x0
@@ -43,7 +43,7 @@ class Line:
          return self.intercept
 
 
-class Corner:
+class intersectPoint:
         
         def __init__(self, l1, l2):
             
@@ -53,7 +53,8 @@ class Corner:
              self.line2 = Line(self.l2.getx0(),self.l2.gety0(),self.l2.getx1(),self.l2.gety1())
              self.poi = None 
              self.angle = None
-             
+             self.isCorner = False
+
              self.xends1 = [self.line1.getx0(), self.line1.getx1()]
              self.yends1 = [self.line1.gety0(), self.line1.gety1()]
              self.xends2 = [self.line2.getx0(), self.line2.getx1()]
@@ -63,11 +64,15 @@ class Corner:
              refS = np.array([[self.line1.getInter()],[self.line2.getInter()]])
              refInv = np.linalg.inv(refA)
              point = np.matmul(refInv, refS)
+             self.theoPoi = point
 
-             if(point[0] >= min(self.xends1) and point[0] <= max(self.xends1) and point[1] >= min(self.yends1) and point[1] <= max(self.yends1) and point[0] >= min(self.xends2) and point[0] <= max(self.xends2) and point[1] >= min(self.yends2) and point[1] <= max(self.yends2)):
+
+
+             if(point[1] >= min(self.xends1) and point[1] <= max(self.xends1) and point[0] >= min(self.yends1) and point[0] <= max(self.yends1) and point[1] >= min(self.xends2) and point[1] <= max(self.xends2) and point[0] >= min(self.yends2) and point[0] <= max(self.yends2)):
             
                 self.poi = point
                 self.angle =  abs(self.l1.getTheta()-self.l2.getTheta())
+                self.isCorner = True
 
 
         def __str__(self):
@@ -81,10 +86,43 @@ class Corner:
         
         def getPoi(self):
                 return self.poi
-         
         
         def getAngle(self):
                 return self.angle
         
         def getAngleInDeg(self):
              return self.angle*180/math.pi
+        
+        def getTheoPoi(self):
+             return self.theoPoi
+        
+        def getCorner(self):
+             return self.isCorner
+
+
+def lineListConstruct(pointsArr):     
+     rows, columns = pointsArr.shape 
+     lineList = []
+     for i in range(rows):
+          newLine = Line(pointsArr[i][0], pointsArr[i][1], pointsArr[i][2], pointsArr[i][3])
+          lineList.append(newLine)
+     return lineList
+
+def intersectListConstruct(linesArr, k, storageArr = None, cornerArr = None):
+     if storageArr is None:
+          storageArr = [] 
+     if cornerArr is None:
+          cornerArr = []
+     if (k == 1):
+          for e in range(len(storageArr)):
+               if storageArr[e].isCorner == True:
+                    cornerArr.append(storageArr[e])
+          return cornerArr
+          
+     else: 
+          for i in range(len(linesArr) - 1):
+               newPoi = intersectPoint(linesArr[0], linesArr[i + 1]) 
+               storageArr.append(newPoi)
+          k -= 1
+          return intersectListConstruct(linesArr[1:], k, storageArr, cornerArr)
+
